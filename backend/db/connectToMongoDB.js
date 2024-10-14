d20828a505b7fac623a791d7c947fbdae10629bf
meet4535 <155011197+meet4535@users.noreply.github.com>
2024-10-15 00:04:39 +0530

    CICD

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -5,7 +5,7 @@ const connectToMongoDB = async () => {
 		await mongoose.connect('mongodb+srv://22IT111:GjGKvQlHY3AlHMHP@cluster0.rmb2bfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 		console.log("Connected to MongoDB");
 	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
+		console.log("Error connecting to MongoDB");
 	}
 };
 
